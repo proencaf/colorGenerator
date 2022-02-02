2223b61acceceef55430ad07534bad9668be8025
proencaf <83221947+proencaf@users.noreply.github.com>
2022-02-02 16:12:39 +0000

    _

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ import SingleColor from './components/SingleColor/SingleColor'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#8d8d19').all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -27,7 +27,7 @@ function App() {
             type='text'
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder='#f15025'
+            placeholder='#8d8d19'
             className={`${error ? 'error' : null}`}
           />
           <button className='btn' type='Submit'>
